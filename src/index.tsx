e9e215d69393f5a1f1845486c65ea66333144d34
guicoelhodev <gs.coelho@outlook.com>
2023-04-16 19:32:00 -0300

    feat: config setup to use in react query

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { router } from "routes";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import store from "store";
 
 const app = document.getElementById("root")!;
+const queryClient = new QueryClient();
 
 createRoot(app).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
-    <GlobalStyle />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <GlobalStyle />
+    </QueryClientProvider>
   </Provider>
 );

add85374d98e315589c7d87bc0d56ad04fca124f
guicoelhodev <gs.coelho@outlook.com>
2023-04-15 23:55:32 -0300

    fix: global style styled-components attr

---
+++ src/style/theme.ts
@@ -0,0 +1,26 @@
+export const themeColors = {
+  blue: "#f00",
+};
+
+export const themeCSS = {
+  "p-sm": "0.5rem",
+  "p-md": "1rem",
+  "p-lg": "1.5rem",
+  "p-xl": "2.5rem",
+
+  "m-sm": "0.5rem",
+  "m-md": "1rem",
+  "m-lg": "1.5rem",
+  "m-xl": "2.5rem",
+
+  "gap-1": "0.5rem",
+  "gap-2": "1rem",
+  "gap-3": "1.5rem",
+  "gap-4": "2.5rem",
+
+  "radius-sm": "0.5rem",
+  "radius-md": "1rem",
+  "radius-lg": "1.5rem",
+  "radius-xl": "2rem",
+  "radius-full": "50%",
+} as const;

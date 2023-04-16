c4cfd68f0b73490c4ce75fd2d8cc6ee26c9893f8
guicoelhodev <gs.coelho@outlook.com>
2023-04-16 19:31:43 -0300

    refactor: remove app.js and change api from js extension to ts

--- src/App/index.js
+++
@@ -1,9 +0,0 @@
-import GlobalStyle from "GlobalStyle/";
-import { Provider } from "react-redux";
-import store from "store/store";
-
-const App = () => {
-  return <GlobalStyle />;
-};
-
-export default App;

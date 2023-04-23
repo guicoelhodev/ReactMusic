d763546569252aaa609588f6dd83340292091f95
guicoelhodev <gs.coelho@outlook.com>
2023-04-22 21:55:22 -0300

    feat: add music list based on get query

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,17 @@
+import { MusicItem } from "components/refactor/MusicItem";
+import { Player } from "components/refactor/Player";
 import { Search } from "components/refactor/Search";
 import React from "react";
 import { IoHeadsetSharp } from "react-icons/io5";
+import { useTopWorldMusic } from "services/http/GET/useTopWorldMusics";
 import * as S from "./style";
 
 export const Home = () => {
+  const { data } = useTopWorldMusic(20);
+
+  const tracks = data?.pages.flat().map((i) => i.tracks)[0].data;
+
+  console.log(tracks);
   return (
     <S.Layout>
       <S.Container>
@@ -16,7 +24,21 @@ export const Home = () => {
           <S.PlayerControls>d</S.PlayerControls>
         </S.Header>
 
-        <Search />
+        <S.Main>
+          <section>
+            <Player />
+          </section>
+
+          <S.MusicNav>
+            <Search />
+
+            <ul>
+              {tracks?.map((track) => (
+                <MusicItem {...track} />
+              ))}
+            </ul>
+          </S.MusicNav>
+        </S.Main>
       </S.Container>
     </S.Layout>
   );
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -4,25 +4,26 @@ export const Layout = styled.div`
   width: 100%;
 `;
 
-export const Container = styled.section`
+export const Container = styled.div`
   width: min(82.25rem, 100%);
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-
-  padding: ${(p) => p.theme["p-lg"]};
 `;
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 0 1rem;
+
+  height: 5rem;
 `;
 
 export const Logo = styled.section`
   display: flex;
   align-items: center;
-  gap: ${(p) => p.theme["gap-2"]};
+  gap: 1rem;
   color: ${(p) => p.theme.colors["purple-800"]};
 
   svg {
@@ -32,3 +33,36 @@ export const Logo = styled.section`
 `;
 
 export const PlayerControls = styled.div``;
+
+export const Main = styled.main`
+  background: aliceblue;
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  height: calc(100vh - 5rem);
+`;
+
+export const MusicNav = styled.section`
+  height: 100%;
+  align-self: flex-start;
+  flex: 1;
+
+  display: grid;
+  grid-template-rows: auto 1fr;
+
+  ul {
+    margin-top: 1rem;
+    height: 100%;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    gap: 1rem;
+    padding: 1rem 0;
+
+    overflow-y: auto;
+  }
+`;

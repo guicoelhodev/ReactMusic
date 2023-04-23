7756f9a871be8fcd21b4c3572f2944eb784dc8b4
guicoelhodev <gs.coelho@outlook.com>
2023-04-23 17:25:06 -0300

    feat: create SearchMusic router

---
+++ src/services/http/GET/useSearchMusics/types.d.ts
@@ -0,0 +1,50 @@
+export type IResponse = {
+  data: Daum[];
+  total: number;
+  next: string;
+};
+
+export type Daum = {
+  id: number;
+  readable: boolean;
+  title: string;
+  title_short: string;
+  title_version: string;
+  link: string;
+  duration: number;
+  rank: number;
+  explicit_lyrics: boolean;
+  explicit_content_lyrics: number;
+  explicit_content_cover: number;
+  preview: string;
+  md5_image: string;
+  artist: Artist;
+  album: Album;
+  type: string;
+};
+
+export type Artist = {
+  id: number;
+  name: string;
+  link: string;
+  picture: string;
+  picture_small: string;
+  picture_medium: string;
+  picture_big: string;
+  picture_xl: string;
+  tracklist: string;
+  type: string;
+};
+
+export type Album = {
+  id: number;
+  title: string;
+  cover: string;
+  cover_small: string;
+  cover_medium: string;
+  cover_big: string;
+  cover_xl: string;
+  md5_image: string;
+  tracklist: string;
+  type: string;
+};

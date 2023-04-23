74a79f1169610898a9b279c864c61556dd107619
guicoelhodev <gs.coelho@outlook.com>
2023-04-23 17:52:45 -0300

    feat: add localStorage to save zustand favorite musics

--- src/zustand/useFavoriteMusicsStore/index.tsx
+++ src/zustand/useFavoriteMusicsStore/index.tsx
@@ -1,25 +1,35 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
 import { IAttributes, IFavoriteMusicsStore } from "./types";
 
 const initialState: IAttributes = {
   favoriteMusics: [],
 };
 
-export const useFavoriteMusicsStore = create<IFavoriteMusicsStore>((set) => ({
-  ...initialState,
+export const useFavoriteMusicsStore = create<IFavoriteMusicsStore>()(
+  persist(
+    (set) => ({
+      ...initialState,
 
-  handleFavoriteMusics: (music) =>
-    set((state) => {
-      let { favoriteMusics } = state;
+      handleFavoriteMusics: (music) =>
+        set((state) => {
+          let { favoriteMusics } = state;
 
-      const alreadyExist = favoriteMusics.some((i) => i.id === music.id);
+          const alreadyExist = favoriteMusics.some((i) => i.id === music.id);
 
-      if (!alreadyExist) {
-        favoriteMusics.push(music);
-      } else {
-        favoriteMusics = favoriteMusics.filter((i) => i.id !== music.id);
-      }
+          if (!alreadyExist) {
+            favoriteMusics.push(music);
+          } else {
+            favoriteMusics = favoriteMusics.filter((i) => i.id !== music.id);
+          }
 
-      return { ...state, favoriteMusics };
+          return { ...state, favoriteMusics };
+        }),
     }),
-}));
+    {
+      name: "@favorite_musics",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
